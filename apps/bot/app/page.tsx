f2657179a9afb359148f5201641b7f5d33a4d5b5
hendraaagil <hendraaagil@gmail.com>
2025-04-25 22:08:05 +0700

    feat: append chat button

--- apps/bot/app/page.tsx
+++ apps/bot/app/page.tsx
@@ -1,12 +1,18 @@
+import { ImageCard } from '@repo/ui/components/image-card'
+import { ChatButton } from '@/components/chat-button'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="max-w-xs p-4">
-        <picture className="w-full">
-          <img src="logo.svg" alt="Aglabs bot logo" />
-        </picture>
+    <main className="mx-auto flex min-h-screen max-w-lg flex-col items-center justify-center space-y-8 px-4">
+      <div className="px-4">
+        <ImageCard
+          imageUrl="logo.svg"
+          caption="Aglabs Bot"
+          className="rounded-full"
+        />
       </div>
-      <h1 className="mt-8 text-center text-4xl font-bold">Aglabs Bot</h1>
+      <h1 className="text-center text-4xl font-bold">Aglabs Bot</h1>
+      <ChatButton />
     </main>
   )
 }
