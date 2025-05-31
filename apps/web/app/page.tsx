694e143cd32558dc4dc93b69fd592636d499956c
hendraaagil <hendraaagil@gmail.com>
2025-05-31 12:11:56 +0700

    perf(web): add fetch priority attribute

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,9 +6,10 @@ export default function Home() {
     <main className="mx-auto flex w-full max-w-lg flex-col items-center justify-center space-y-8 px-4 py-8">
       <div className="px-4">
         <ImageCard
-          imageUrl="logo-primary.svg"
-          caption="Aglabs Dev"
+          src="logo-primary.svg"
+          alt="Aglabs Dev"
           className="max-w-3xs rounded-full"
+          fetchPriority="high"
         />
       </div>
       <h1 className="mt-8 text-center text-4xl font-bold">Aglabs Dev</h1>
