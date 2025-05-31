9a9552b28f1eb974cb2c6d841242612cd9eb456b
hendraaagil <hendraaagil@gmail.com>
2025-05-31 12:11:49 +0700

    perf(bot): add fetch priority attribute

--- apps/bot/app/page.tsx
+++ apps/bot/app/page.tsx
@@ -6,9 +6,10 @@ export default function Home() {
     <main className="mx-auto flex w-full max-w-lg flex-col items-center justify-center space-y-8 px-4 py-8">
       <div className="px-4">
         <ImageCard
-          imageUrl="logo.png"
-          caption="Aglabs Bot"
+          src="logo.png"
+          alt="Aglabs Bot"
           className="max-w-3xs rounded-full"
+          fetchPriority="high"
         />
       </div>
       <h1 className="text-center text-4xl font-bold">
