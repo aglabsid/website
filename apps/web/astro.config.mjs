16807bc30fce35a5b620a4e3ab9d8cc8e1ff5443
hendraaagil <hendraaagil@gmail.com>
2026-04-07 16:07:22 +0700

    chore: fix site url

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+  site: 'https://aglabs.id',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
